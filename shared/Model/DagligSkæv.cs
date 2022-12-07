c8dae3450fc601e8e1b0e5724849cbcfdb69ec8d
danieldawsonjensen <dani540c@gmail.com>
2022-12-07 09:02:45 +0100

    mstadig goat

--- shared/Model/DagligSkæv.cs
+++ shared/Model/DagligSkæv.cs
@@ -1,32 +1,44 @@
 namespace shared.Model;
 
-public class DagligSkæv : Ordination {
+public class DagligSkæv : Ordination
+{
     public List<Dosis> doser { get; set; } = new List<Dosis>();
 
-    public DagligSkæv(DateTime startDen, DateTime slutDen, Laegemiddel laegemiddel) : base(laegemiddel, startDen, slutDen) {
-	}
-
-    public DagligSkæv(DateTime startDen, DateTime slutDen, Laegemiddel laegemiddel, Dosis[] doser) : base(laegemiddel, startDen, slutDen) {
-        this.doser = doser.ToList();
-    }    
-
-    public DagligSkæv() : base(null!, new DateTime(), new DateTime()) {
+    public DagligSkæv(DateTime startDen, DateTime slutDen, Laegemiddel laegemiddel) : base(laegemiddel, startDen, slutDen)
+    {
     }
 
-	public void opretDosis(DateTime tid, double antal) {
+    public DagligSkæv(DateTime startDen, DateTime slutDen, Laegemiddel laegemiddel, Dosis[] doser) : base(laegemiddel, startDen, slutDen)
+    {
+        this.doser = doser.ToList();
+    }
+
+    public DagligSkæv() : base(null!, new DateTime(), new DateTime())
+    {
+    }
+
+    public void opretDosis(DateTime tid, double antal)
+    {
         doser.Add(new Dosis(tid, antal));
     }
 
-	public override double samletDosis() {
-		return base.antalDage() * doegnDosis();
-	}
+    public override double samletDosis()
+    {
+        return base.antalDage() * doegnDosis();
+    }
 
-	public override double doegnDosis() {
-		// TODO: Implement!
-        return -1;
-	}
+    public override double doegnDosis()
+    {
+        double sum = 0;
+        foreach (var dosis in doser)
+        {
+            sum = sum + dosis.antal;
+        }
+        return sum;
+    }
 
-	public override String getType() {
-		return "DagligSkæv";
-	}
+    public override String getType()
+    {
+        return "DagligSkæv";
+    }
 }
